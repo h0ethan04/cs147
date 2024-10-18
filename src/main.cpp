451eaf817fc22aed5281b86112ec7d089b2d1322
Ethan Ho <hoea2@uci.edu>
2024-10-18 10:32:06 -0700

    servo runs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <Arduino.h>
 #include <Servo.h>
+constexpr uint8_t servoPort = 13;
+constexpr uint8_t analogReadPort = 32;
+Servo servo;
 
 void setup() {
+    // pinMode(15, OUTPUT);
+    Serial.begin(9600);
+    // pinMode(servoPort, OUTPUT);
+    servo.attach(servoPort);
 
 }
 
 void loop() {
+    // servo.write(100);
+    Serial.print(analogRead(analogReadPort));
+    sleep(1);
+    for (int i = 0; i < 45; ++i) {
+        servo.write(((servo.read()) + i) % 180);
+        // Serial.println("loop");
+        sleep(1);
+    }
+    // servo.write(100);
     
 }
