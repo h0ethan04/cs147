9090702ce40f138799961e270c85182e0cd7e414
Brian Le <bkylele@gmail.com>
2024-10-18 11:20:38 -0700

    Changed LED logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@
 
 constexpr uint8_t servoPort = 13;
 constexpr uint8_t analogReadPort = 32;
-constexpr uint8_t ledPin = 15;
 constexpr uint32_t calibrationTime = 10000;
 Servo servo;
+
+constexpr uint8_t ledPin = 2;
+constexpr uint32_t ledDelay = 1000;
+uint8_t ledState = LOW;
+uint32_t lastLedTime = 0;
 
 uint32_t minLightLevel, maxLightLevel;
 
@@ -23,19 +27,19 @@ void setup() {
     uint32_t begin = millis();
     uint32_t end = begin;
     while (end - begin <= calibrationTime) {
-        // Blink LED
-        digitalWrite(ledPin, HIGH);
-
         // Keep track of min and max light values
         uint16_t value = analogRead(analogReadPort);
         minLightLevel = min(minLightLevel, value);
         maxLightLevel = max(maxLightLevel, value);
 
-        delay(250);
-        digitalWrite(ledPin, LOW);
-        delay(250);
-
+        delay(500);
         end = millis();
+
+        if (end - lastLedTime > ledDelay) {
+            lastLedTime = end;
+            ledState = !ledState;
+            digitalWrite(ledPin, ledState);
+        }
     }
 
     // LED off to finish calibration
