7f31d426147029aa5f4e58a20b1efbbc7f127317
Brian Le <bkylele@gmail.com>
2024-10-25 11:24:08 -0700

    Part 1 code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include <TFT_eSPI.h>
+
+constexpr int FG = TFT_WHITE;
+constexpr int BG = TFT_BLACK;
+
+TFT_eSPI tft = TFT_eSPI();
+uint32_t time_elapsed;
 
 void setup() {
+    tft.init();
+    tft.setRotation(1); // 0 for portrait, 1 for landscape
+    tft.fillScreen(BG);
 
+    tft.setTextColor(FG, BG);
+    // tft.setTextSize(1); // Default text size is 1
+
+    tft.setCursor(0,0);
 }
 
 void loop() {
-    
+    for (;;) {
+        tft.fillRect(0, 0, 80, 16, BG);
+        tft.print(time_elapsed++);
+        delay(1000);
+    }
 }
