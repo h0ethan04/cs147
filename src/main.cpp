83fecf04577b1ad44ba530c28985443cc216edcc
Ethan Ho <hoea2@uci.edu>
2024-10-29 01:37:58 -0700

    nah i'd win

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,21 @@
 #include <TFT_eSPI.h>
 #include <Adafruit_CAP1188.h>
 
-constexpr int CAP1188_RESET = 13;
-Adafruit_CAP1188 cap = Adafruit_CAP1188();
+#define CAP1188_RESET 13
 
+#define CLKPIN 22
+#define MISOPIN 21
+#define MOSIPIN 17
+#define CSPIN 15
+// hardware i2c
+// sda 21
+// sck 22
+// vin 3v
+Adafruit_CAP1188 cap = Adafruit_CAP1188();
+// software spi
+// ad ground
+// 3vo 3v
+// Adafruit_CAP1188 cap(CLKPIN, MISOPIN, MOSIPIN, CSPIN, CAP1188_RESET);
 
 constexpr int FG = TFT_WHITE;
 constexpr int BG = TFT_BLACK;
@@ -16,12 +28,15 @@ uint32_t time_elapsed;
 void setup() {
     Serial.begin(9600);
 
+    delay(1500); // delay so we don't miss serial msgs
+
+    Serial.println("\nStarting CAP1188 check");
+
     if (!cap.begin()) {
         Serial.println("CAP1188 not found");
-        for (;;);
+        for (;;); // what the fuck is this brian
     }
     Serial.println("CAP1188 found!");
-
 
     // Initialize OLED display
     tft.init();
@@ -35,23 +50,16 @@ void setup() {
 }
 
 void loop() {
-    // uint8_t touched = cap.touched();
-    // if (touched == 0) {
-    //     // No touch detected
-    //     return;
-    // }
+    uint8_t total = 0;
+    uint8_t touched = cap.touched();
+    if (!touched) return;
 
-    // for (uint8_t i=0; i<8; i++) {
-    //     if (touched & (1 << i)) {
-    //         Serial.print("C");
-    //         Serial.print(i+1);
-    //         Serial.print("\t");
-    //     }
-    // }
-    // Serial.println();
+    for (uint8_t i = 0; i < 8; i++) {
+        if (touched & (1 << i)) ++total;
+    }
 
     tft.fillRect(0, 0, 80, 16, BG);
-    tft.setCursor(0,65);
-    tft.print(time_elapsed++);
-    delay(1000);
+    tft.setCursor(0, 65);
+    tft.print(total);
+    delay(100);
 }
