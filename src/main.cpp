cfa6a0a008e2d31b2f314a2b9f103f7815fc7483
Brian Le <bkylele@gmail.com>
2024-11-21 16:00:17 -0800

    Part 2 beginning template

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <Arduino.h>
+#include <HttpClient.h>
+#include <WiFi.h>
 #include <inttypes.h>
 #include <stdio.h>
+
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs.h"
 #include "nvs_flash.h"
 
-void setup() {
-    Serial.begin(9600);
-    delay(1000);
 
+// This example downloads the URL "http://arduino.cc/"
+char ssid[50]; // your network SSID (name)
+char pass[50]; // your network password (use for WPA, or use
+               // as key for WEP)
+
+// Name of the server we want to connect to
+const char kHostname[] = "worldtimeapi.org";
+// Path to download (this is the bit after the hostname in the URL
+// that you want to download
+const char kPath[] = "/api/timezone/Europe/London.txt";
+
+// Number of milliseconds to wait without receiving any data before we give up
+const int kNetworkTimeout = 30 * 1000;
+// Number of milliseconds to wait if no data is available before trying again
+const int kNetworkDelay = 1000;
+
+void nvs_access() {
     // Initialize NVS
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
@@ -22,6 +39,7 @@ void setup() {
     }
     ESP_ERROR_CHECK(err);
 
+    // Open
     Serial.printf("\n");
     Serial.printf("Opening Non-Volatile Storage (NVS) handle... ");
     nvs_handle_t my_handle;
@@ -30,30 +48,119 @@ void setup() {
 	Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
     } else {
 	Serial.printf("Done\n");
-
-	// Write
-	Serial.printf("Updating ssid/pass in NVS ... ");
-	char ssid[] = "YOUR_SSID";
-	char pass[] = "YOUR_PASSWD";
-	err = nvs_set_str(my_handle, "ssid", ssid);
-	err |= nvs_set_str(my_handle, "pass", pass);
-	Serial.printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
-
-	// Commit written value.
-	// After setting any values, nvs_commit() must be called to ensure changes
-	// are written to flash storage. Implementations may write to storage at
-	// other times, but this is not guaranteed.
-	Serial.printf("Committing updates in NVS ... ");
-	err = nvs_commit(my_handle);
-	Serial.printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
-
-	// Close
-	nvs_close(my_handle);
+	Serial.printf("Retrieving SSID/PASSWD\n");
+	size_t ssid_len;
+	size_t pass_len;
+	err = nvs_get_str(my_handle, "ssid", ssid, &ssid_len);
+	err |= nvs_get_str(my_handle, "pass", pass, &pass_len);
+	switch (err) {
+	case ESP_OK:
+	    Serial.printf("Done\n");
+	    //Serial.printf("SSID = %s\n", ssid);
+	    //Serial.printf("PASSWD = %s\n", pass);
+	    break;
+	case ESP_ERR_NVS_NOT_FOUND:
+	    Serial.printf("The value is not initialized yet!\n");
+	    break;
+	default:
+	    Serial.printf("Error (%s) reading!\n", esp_err_to_name(err));
+	}
     }
+
+    // Close
+    nvs_close(my_handle);
 }
 
+void setup() {
+    Serial.begin(9600);
+    delay(1000);
+    // Retrieve SSID/PASSWD from flash before anything else
+    nvs_access();
+
+    // We start by connecting to a WiFi network
+    delay(1000);
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+	delay(500);
+	Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("MAC address: ");
+    Serial.println(WiFi.macAddress());
+}
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    delay(1000);
+    int err = 0;
+
+    WiFiClient c;
+    HttpClient http(c);
+
+    err = http.get(kHostname, kPath);
+    if (err == 0) {
+	Serial.println("startedRequest ok");
+
+	err = http.responseStatusCode();
+	if (err >= 0) {
+	    Serial.print("Got status code: ");
+	    Serial.println(err);
+
+	    // Usually you'd check that the response code is 200 or a
+	    // similar "success" code (200-299) before carrying on,
+	    // but we'll print out whatever response we get
+
+	    err = http.skipResponseHeaders();
+	    if (err >= 0) {
+		int bodyLen = http.contentLength();
+		Serial.print("Content length is: ");
+		Serial.println(bodyLen);
+		Serial.println();
+		Serial.println("Body returned follows:");
+
+		// Now we've got to the body, so we can print it out
+		unsigned long timeoutStart = millis();
+		char c;
+		// Whilst we haven't timed out & haven't reached the end of the body
+		while ((http.connected() || http.available()) &&
+		       ((millis() - timeoutStart) < kNetworkTimeout)) {
+		    if (http.available()) {
+			c = http.read();
+			// Print out this character
+			Serial.print(c);
+
+			bodyLen--;
+			// We read something, reset the timeout counter
+			timeoutStart = millis();
+		    } else {
+			// We haven't got any data, so let's pause to allow some to
+			// arrive
+			delay(kNetworkDelay);
+		    }
+		}
+	    } else {
+		Serial.print("Failed to skip response headers: ");
+		Serial.println(err);
+	    }
+	} else {
+	    Serial.print("Getting response failed: ");
+	    Serial.println(err);
+	}
+    } else {
+	Serial.print("Connect failed: ");
+	Serial.println(err);
+    }
+    http.stop();
+
+    // And just stop, now that we've tried a download
+    while (1)
+	;
 }
