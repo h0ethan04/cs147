69f564ea263f95f428a6d57808e5ddd1d1fdb3bc
Ethan Ho <hoea2@uci.edu>
2024-11-26 23:54:14 -0800

    fix status code

--- server/routes/tempReadings.mjs
+++ server/routes/tempReadings.mjs
@@ -6,13 +6,13 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   const tempReadings = await db.collection('temp_readings').find({}).toArray();
-  res.send(tempReadings).status(200);
+  res.status(200).send(tempReadings);
 });
 
 router.post('/', async (req, res) => {
   const {tempReading, gasReading} = req.body;
   await db.collection('temp_readings').insertOne({tempReading: tempReading, gasReading: gasReading, date: new Date()});
-  res.send(tempReading).status(201);
+  res.status(201).send({tempReading, gasReading});
 });
 
 export default router;
